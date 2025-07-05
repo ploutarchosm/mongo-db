6dc8a5ee626836fea8fd2e755efd87bdde6d0a49
Ploutarchos Michaelides <info@ploutarchosm.com>
2025-07-05 17:00:26 +0300

    fix bug error for global module

--- src/lib/mongo-db.module.ts
+++ src/lib/mongo-db.module.ts
@@ -1,16 +1,18 @@
-import { Module } from '@nestjs/common';
+import { Global, Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
-import { MongodbConfigService } from "./mongodb-config.service";
+import { MongodbConfigService } from './mongodb-config.service';
 
+@Global()
 @Module({
-    imports: [
-        MongooseModule.forRootAsync({
-            useFactory: (mongodbConfig: MongodbConfigService) => ({
-                uri: mongodbConfig.uri
-            }),
-            inject: [MongodbConfigService],
-        }),
-    ],
-    providers: [MongodbConfigService], // Remove MongooseModule from here
+  imports: [
+    MongooseModule.forRootAsync({
+      useFactory: (mongodbConfig: MongodbConfigService) => ({
+        uri: mongodbConfig.uri,
+      }),
+      inject: [MongodbConfigService],
+    }),
+  ],
+  providers: [MongodbConfigService],
+  exports: [MongodbConfigService],
 })
 export class MongoDbModule {}
